1a272f240178ad876e989a1a19cf6c3cc7d04533
Victor Raposo <vwraposo@gmail.com>
2020-01-27 16:46:08 -0300

    small fixes

--- node/middlewares/internals/utils.ts
+++ node/middlewares/internals/utils.ts
@@ -1,6 +1,6 @@
 export const tenMinutesFromNowMS = () =>
   `${new Date(Date.now() + 10 * 60 * 1000)}`
-export const STORE_LOCATOR = 'vtex.store@2.85.1-beta.0'
+export const STORE_LOCATOR = 'vtex.store@2.x'
 export const ROUTES_JSON_PATH = 'dist/vtex.store-indexer/build.json'
 export const PAGE_TYPES = {
   ['PRODUCT']: 'product',
@@ -8,7 +8,7 @@ export const PAGE_TYPES = {
   ['CATEGORY']: 'category',
   ['SUBCATEGORY']: 'subcategory',
   ['DEPARTMENT']: 'department',
-  ['PRODUCT_NOT_FOUND']: 'notFound',
+  ['PRODUCT_NOT_FOUND']: 'productNotFound',
 }
 
 export const slugify = (str: string) =>
