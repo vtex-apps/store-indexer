1537e854c284279c8804ee50a47eb4faf19dc73f
Victor Raposo <vwraposo@gmail.com>
2020-02-05 10:18:36 -0300

    wip

--- node/resources/idUrlIndex.ts
+++ node/resources/idUrlIndex.ts
@@ -1,17 +1,22 @@
 import { VBase } from '@vtex/api'
+import { RedirectInput, RedirectTypes } from 'vtex.rewriter'
+
+import { RewriterGraphql } from '../clients/rewriterGraphql'
 
 const INDEX = 'INDEX'
 const REVERSE_INDEX = 'ID_TO_URL'
 
 const toReverseIndexKey = (id: string, binding: string) => `${id}__${binding}`
-
+// TODO receives multiple bindings
 export class IdUrlIndex {
   private bucket: string
-  constructor(protected vbase: VBase) {
+  constructor(
+    protected vbase: VBase,
+    protected rewriterGraphql: RewriterGraphql
+  ) {
     this.bucket = `_${INDEX}_${REVERSE_INDEX}`
   }
 
-  // Additional methods for indexing of URLs by their corresponding ID in VTEX catalog.
   public get = async (id: string, binding: string) => {
     const key = toReverseIndexKey(id, binding)
     const response = await this.vbase.getJSON<string>(this.bucket, key, true)
@@ -19,6 +24,22 @@ export class IdUrlIndex {
   }
 
   public save = async (id: string, binding: string, url: string) => {
+    const canonical = await this.get(id, binding)
+    if (canonical && canonical !== url) {
+      const { id: canonicalId } = (await this.rewriterGraphql.getInternal(
+        url
+      )) || { id: null }
+      if (canonicalId !== id) {
+        const redirect: RedirectInput = {
+          bindings: [binding],
+          from: canonical,
+          to: url,
+          // endDate TODO ONE YEAR???
+          type: RedirectTypes.Temporary,
+        }
+        await this.rewriterGraphql.saveRedirect(redirect)
+      }
+    }
     const key = toReverseIndexKey(id, binding)
     const response = await this.vbase.saveJSON<string>(this.bucket, key, url)
     return response
