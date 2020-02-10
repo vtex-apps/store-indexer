08279aa98786cdf675e9739af3f0b398a404832b
Victor Raposo <vwraposo@gmail.com>
2020-02-10 12:16:08 -0300

    Adds special characters to url building regex

--- node/middlewares/internals/utils.ts
+++ node/middlewares/internals/utils.ts
@@ -19,7 +19,7 @@ export const slugify = (str: string) =>
     .toLowerCase()
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '')
-    .replace(/&|\s+/g, '-')
+    .replace(/[*+~.()'"!:@&\[\]`,/ %$#?{}|><=_^]/g, '-')
 
 export interface ContentTypeDefinition {
   internal: string
