6323bb1f8feeb17d6007a795d081fe3f7f36eb36
Jeymisson Oliveira <jeymissoncz@gmail.com>
2020-02-17 16:28:19 -0300

    Fix typing for toInternalURL

--- node/middlewares/search/indexCanonicals.ts
+++ node/middlewares/search/indexCanonicals.ts
@@ -5,7 +5,7 @@ import { InternalInput } from 'vtex.rewriter'
 
 import { Catalog, CatalogPageTypeResponse } from '../../clients/catalog'
 import { ColossusEventContext } from '../../typings/Colossus'
-import { PAGE_TYPES, PAGE_TYPE_TO_STORE_ENTITIES } from '../internals/utils'
+import { PAGE_TYPE_TO_STORE_ENTITIES } from '../internals/utils'
 
 const BUCKET_SIZE = 100
 const DAYS_TO_EXPIRE = 7
@@ -55,14 +55,14 @@ const toInternalURL = async (
   canonicalPath: string,
   endDate: string,
   catalog: Catalog
-) => {
+): Promise<InternalInput | null> => {
   const [path, query] = uri.split('?')
   const parsedQuery = querystring.parse(query)
   const { pageType } = await getPageType(path, `?${query}`, catalog)
   const type = PAGE_TYPE_TO_STORE_ENTITIES[pageType]
 
   if(!type){
-    return 
+    return null
   }
 
   return {
@@ -88,8 +88,8 @@ export async function indexCanonicals(
   const endDate = new Date()
   endDate.setDate(endDate.getDate() + DAYS_TO_EXPIRE)
   for (const URLsBucket of buckets) {
-    const internals = (await Promise.all(
-      URLsBucket.map<Promise<InternalInput>>(url =>
+    const internals = await Promise.all(
+      URLsBucket.map<Promise<InternalInput| null>>(url =>
         toInternalURL(
           url.path,
           url.canonicalPath!,
@@ -97,8 +97,10 @@ export async function indexCanonicals(
           catalog
         )
       )
-    )).filter(internal => internal)
-    await rewriterGraphql.saveManyInternals(internals)
+    )
+    await rewriterGraphql.saveManyInternals(
+      internals.filter(internal => internal !== null) as InternalInput[]
+    )
   }
   await next()
 }
