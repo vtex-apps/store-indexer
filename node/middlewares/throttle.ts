a3baafdfa16052d2889887982901182d146bdbdf
Victor Raposo <vwraposo@gmail.com>
2020-03-19 11:17:36 -0300

    Decreases  number of maximun simultaneous requests

--- node/middlewares/throttle.ts
+++ node/middlewares/throttle.ts
@@ -2,7 +2,7 @@ import { TooManyRequestsError } from '@vtex/api'
 
 import { ColossusEventContext } from '../typings/Colossus'
 
-const MAX_REQUEST = 100
+const MAX_REQUEST = 50
 let COUNTER = 0
 
 export async function throttle(
