0ace17ddfe03a238f7fc25bd383ad33ddc5bd33a
Tiago Gimenes <tlgimenes@gmail.com>
2020-04-03 06:40:20 -0300

    changelog

--- node/middlewares/tenant.ts
+++ node/middlewares/tenant.ts
@@ -2,7 +2,10 @@ import { ColossusEventContext } from '../typings/global'
 
 const TEN_MINUTES_S = 10 * 60
 
-export async function tenant(ctx: Context, next: () => Promise<void>) {
+export async function tenant(
+  ctx: ColossusEventContext,
+  next: () => Promise<void>
+) {
   const {
     clients: { tenant: tenantClient },
   } = ctx
