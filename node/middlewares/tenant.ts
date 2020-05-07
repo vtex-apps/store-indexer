73aa0f54c9e6b7ecaf81526f6de0abdec9771a69
Victor Raposo <vwraposo@gmail.com>
2020-05-07 10:01:31 -0300

    Use 404 from the tenantclient

--- node/middlewares/tenant.ts
+++ node/middlewares/tenant.ts
@@ -1,5 +1,3 @@
-import { NotFoundError } from '@vtex/api'
-
 import { Context } from '../typings/global'
 
 const TEN_MINUTES_S = 10 * 60
@@ -7,17 +5,11 @@ const TEN_MINUTES_S = 10 * 60
 export async function tenant(ctx: Context, next: () => Promise<void>) {
   const {
     clients: { tenant: tenantClient },
-    vtex: { account, logger },
   } = ctx
   const tenantInfo = await tenantClient.info({
     forceMaxAge: TEN_MINUTES_S,
-    nullIfNotFound: true,
   })
-  if (!tenantInfo) {
-    logger.error(`Account ${account} not in tenant system`)
-    ctx.body = `Account ${account} not in tenant system`
-    throw NotFoundError
-  }
+
   const locale = tenantInfo.defaultLocale
 
   ctx.state.tenantInfo = tenantInfo
